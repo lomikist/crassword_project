9480afce78916e905aa36b2620e37099c795515b
lomikist <59786804+lomikist@users.noreply.github.com>
2023-09-26 12:46:39 +0400

    s

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,8 @@
-#include "main.h"
+// #include "../include/main.h"
 
-// MAIN File
+int main(int argc, char *argv[]){
+	// display welcome message
+	// std::cout << "\n\n------------------------------------------------" << std::endl;
+	// std::cout << "Welcome to the Crossword Anagram Puzzle Program" << std::endl;
+	// std::cout << "------------------------------------------------\n\n" << std::endl;
+}
