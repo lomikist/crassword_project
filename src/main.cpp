c225df52225d42430dcd32f37fd2d1c3739644c9
lomikist <59786804+lomikist@users.noreply.github.com>
2023-09-26 12:48:15 +0400

    s

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
-// #include "../include/main.h"
+#include "../include/main.h"
 
 int main(int argc, char *argv[]){
-	// display welcome message
-	// std::cout << "\n\n------------------------------------------------" << std::endl;
-	// std::cout << "Welcome to the Crossword Anagram Puzzle Program" << std::endl;
-	// std::cout << "------------------------------------------------\n\n" << std::endl;
+	display welcome message
+	std::cout << "\n\n------------------------------------------------" << std::endl;
+	std::cout << "Welcome to the Crossword Anagram Puzzle Program" << std::endl;
+	std::cout << "------------------------------------------------\n\n" << std::endl;
 }
